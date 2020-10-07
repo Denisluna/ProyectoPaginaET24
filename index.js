1c084c7e3c691c403a286c65099ca640e59d75fd
Denis Luna <erickdenizluna@hotmail.com>
2020-10-07 20:48:02 -0300

    diseño Especialidades

--- index.js
+++ index.js
@@ -1,38 +1,23 @@
 const http = require('http');
-const fs = require('fs');
 const express = require('express');
+const bcrypt = require('bcrypt');
+const instRouter = require('./institucionalRouter');
 const app = express();
 const server = http.createServer(app);
 
-const PORT = 3000;
+const PORT = 8080;
 
 app.set('view engine', 'jade');
 
+app.use(express.urlencoded({extended: false}));
 app.use('/public', express.static('public'));
+app.use('/institucional', instRouter);
 
 app.get('/',(req, res) => {
   res.render('inicio');
 });
 app.get('/historia',(req, res) => {
   res.render('historia');
-});
-app.get('/institucional/especialidades',(req, res) => {
-  res.render('institucional/especialidades');
-});
-app.get('/institucional/proyectos-escolares',(req, res) => {
-  res.render('institucional/proy');
-});
-app.get('/institucional/vision',(req, res) => {
-  res.render('institucional/vision');
-});
-app.get('/institucional/equipo-docente',(req, res) => {
-  res.render('institucional/equipod');
-});
-app.get('/institucional/regimen-evaluacion',(req, res) => {
-  res.render('institucional/reg');
-});
-app.get('/institucional/becas',(req, res) => {
-  res.render('institucional/becas');
 });
 app.get('/talleres/aleman',(req, res) => {
   res.render('talleres/aleman');
@@ -43,6 +28,12 @@ app.get('/talleres/club-exploradores',(req, res) => {
 app.get('/bi',(req, res) => {
   res.render('bi');
 });
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+app.get('/register', (req, res) => {
+  res.render('register');
+})
 
 server.listen(PORT, () => {
   console.log("server escuchando en el puerto *", PORT);
---
+++ institucionalRouter.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/especialidades',(req, res) => {
+  res.render('institucional/especialidades');
+});
+router.get('/proyectos-escolares',(req, res) => {
+  res.render('institucional/proy');
+});
+router.get('/vision',(req, res) => {
+  res.render('institucional/vision');
+});
+router.get('/equipo-docente',(req, res) => {
+  res.render('institucional/equipod');
+});
+router.get('/regimen-evaluacion',(req, res) => {
+  res.render('institucional/reg');
+});
+router.get('/becas',(req, res) => {
+  res.render('institucional/becas');
+});
+
+module.exports = router;
